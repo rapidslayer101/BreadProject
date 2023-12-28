309326fb4d3c67f81dbe58e2ea5eecbf64bae8a5
Jake <49795711+Rarisma@users.noreply.github.com>
2023-12-28 09:52:06 +0000

    add ai  dal

--- firehose.py
+++ firehose.py
@@ -1,24 +1,27 @@
 # Firehose - A mass media monitoring tool.
-import feedparser
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 import time
 import requests
 import re
-    # === CONFIG ===
+import AIDataAccessLayer as AIDAL
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime, timedelta
+import feedparser
+
+# === CONFIG ===
 DateCutoff = 2  # Don't parse stories older than x days.
 
 # Sources is a set to prevent dupe issues
 sources = set([])
 blacklist = set([])
-
 """
 This is a dictionary in the format of 
 Key - URL
 Value - [Title, Publish date, Article Text]
 """
 pending_stories = dict()
-
+read_stories = []
 def parse_blacklist(path):
     global blacklist
     # Read the entire content of the file
@@ -40,30 +43,44 @@ def parse_sources(path):
     print(f"RSS Feed parsing complete, loaded {len(sources)} sources.")
 
 
-def get_stories():
+def parse_feed(url):
     """
-    Loads all stories from the source list.
+    Parse individual feed and return stories.
     """
     current_time = datetime.now()
-    for url in sources:
-        feed = feedparser.parse(url)
-        feed_title = feed.feed.get('title', 'No Title Available')
-        print(f"\nParsing feed: {feed_title} ({url})")
+    stories = []
+    feed = feedparser.parse(url)
+    feed_title = feed.feed.get('title', 'No Title Available')
+    print(f"\nParsing feed: {feed_title} ({url})")
 
-        for entry in feed.entries:
-            published_str = entry.get('published', '')
-            published_date = parse_published_date(published_str)
+    for entry in feed.entries:
+        published_str = entry.get('published', '')
+        published_date = parse_published_date(published_str)
 
-            # Do not parse stories that are beyond the cut off point
-            if published_date:
-                if current_time - published_date >= timedelta(days=DateCutoff):
-                    continue
+        if published_date and current_time - published_date < timedelta(days=DateCutoff):
+            story_url = entry.get('link', "ERR")
+            if story_url != "ERR" and story_url not in pending_stories:
+                stories.append((story_url, entry.get('title', 'Title Unavailable'), published_str, get_article_text(story_url)))
+                print(f"found story - {stories[-1][1]}")
 
-            # Check if the story's URL is new and add it to pending_stories if it is.
-            url = entry.get('link', "ERR")
-            if url != "ERR" and url not in pending_stories:
-                pending_stories[url] = (entry.get('title', 'Title Unavailable'), published_str, get_article_text(url))
-                print(f"found story - {pending_stories[url][0]}   ")
+    return stories
+
+def get_stories():
+    """
+    Load all stories from the source list using multithreading.
+    """
+    with ThreadPoolExecutor() as executor:
+        future_to_url = {executor.submit(parse_feed, url): url for url in sources}
+        for future in as_completed(future_to_url):
+            url = future_to_url[future]
+            try:
+                stories = future.result()
+                for story in stories:
+                    url = story[0]
+                    if url not in pending_stories:
+                        pending_stories[url] = story[1:]
+            except Exception as exc:
+                print(f'{url} generated an exception: {exc}')
 
 
 def get_article_text(url):
@@ -102,6 +119,15 @@ if __name__ == "__main__":
     parse_blacklist("Blacklist.txt")
     print(f"{len(blacklist)} blacklist items")
     get_stories()
-    #print(pending_stories)
     print(f"Found {len(pending_stories)} ready to be parsed!")
-    print(f"This took {time.time() - start}")
+    print(f"This took {time.time() - start} seconds!")
+    client = AIDAL.init()
+    print("parsing with AI")
+    for story in pending_stories.keys():
+        content = pending_stories[story]
+        AI_response = AIDAL.summarise_article(client, (content[0] + "\n" + content[1] + "\n" + content[2]))
+        print(AI_response)
+        read_stories.append([story, content[0], content[1], content[2], AI_response])
+
+
+
