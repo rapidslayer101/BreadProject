6d87ce513b7cfac3bd502f247cf98aac9c1d78a9
Rapid <rapidslayer101@gmail.com>
2024-01-01 00:58:20 +0000

    Small push with start of TNS and deleted redundant file

--- ticker_loader.py
+++ ticker_loader.py
@@ -2,7 +2,6 @@ from os.path import exists
 from os import mkdir
 from tickers_and_indexes import *
 from datetime import datetime, timedelta
-from options import *
 
 # File to load TICKERS, INDEXES and COMPANIES so that AI can link them together
 # This file should be called when the program starts
@@ -58,6 +57,23 @@ def refresh_ticker_data(file, refresh_days):
                     ticker_info.append(ticker.replace("\n", "").split("§"))
     return ticker_info
 
+
+def refresh_lse_tickers():
+    if not exists(f"TickerData/lse.txt"):
+        if not exists(f"TickerData/lse.xlsx"):
+            print("LSE tickers not found, please download the file from "
+                  "https://www.londonstockexchange.com/reports?tab=instruments, then save it as lse.xlsx in the "
+                  "TickerData folder")
+            exit()
+        else:
+            with open(f"TickerData/lse.xlsx", "r", encoding="utf-8") as f:
+                data = f.readlines()
+                print(data)
+                # todo load lse.xlsx and write to lse.txt
+    else:
+        print("x")
+        # todo load lse.txt and check for refresh requirements
+
 # type ticker: [ticker, company/type (e.g. bond, etf), other data]
 # type index: [ticker, company, other data]
 # type weighted index: [ticker, company, weight, other data]
@@ -70,10 +86,47 @@ dow_jones = refresh_ticker_data("dow_jones", 7)  # type weighted index
 nifty50 = refresh_ticker_data("nifty50", 7)  # type index
 ftse100 = refresh_ticker_data("ftse100", 7)  # type index
 ftse250 = refresh_ticker_data("ftse250", 7)  # type index
+lse = refresh_lse_tickers()  # type tickers
+
+# todo UK tickers list  -- https://www.londonstockexchange.com/reports?tab=instruments
+# the UK stock list is updated once a month in the first week of the month
+
+tickers = [nasdaq, nasdaq_other, lse]
+indexes = {'sp_500': sp_500, 'dow_jones': dow_jones, 'nifty50': nifty50, 'ftse100': ftse100, 'ftse250': ftse250}
+
+
+input()
+def tns(name):  # ticker name system  # todo work in progress
+    name = name.lower()
+    related_tickers = []
+    for ticker_list in tickers:
+        for ticker in ticker_list:
+            if f"{name} " in ticker[1].lower():
+                related_tickers.append([ticker[0], ticker[1]])
+            if f"{name}, " in ticker[1].lower():
+                related_tickers.append([ticker[0], ticker[1]])
+
+    relevant_indexes = []
+    for ticker in related_tickers:
+        for key in indexes.keys():
+            for _ticker in indexes[key]:
+                if ticker[0] == _ticker[0]:
+                    relevant_indexes.append(key)
+    return related_tickers, relevant_indexes
+
+
+print(tns("Tesla"))
+print(tns("Microsoft"))
+print(tns("Apple"))
+print(tns("Alphabet"))
+print(tns("Amazon"))
+print(tns("Rolls-Royce"))
+
+
 
 #print(get_ticker_data("tsla"))
 #print(get_ticker_stats("tsla"))
-print(get_ticker_profile("tsla"))
+#print(get_ticker_profile("tsla"))
 
 #print(get_ticker_history("tsla", datetime.now()-timedelta(days=1), datetime.now(),
 #                         "1d", "1m"))
