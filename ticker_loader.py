5c148e2fe6914354b48e7e04dcc53b6d6ad35e24
Rapid <rapidslayer101@gmail.com>
2024-01-07 23:56:51 +0000

    Moved files around, added TNS_check function

--- ticker_loader.py
+++ ticker_loader.py
@@ -29,7 +29,7 @@ indexes = {'sp_500': sp_500, 'dow_jones': dow_jones, 'nifty50': nifty50, 'ftse10
 print("Loaded tickers and indexes successfully...\n-------------------------------------------")
 
 
-def tns(name):  # ticker name system  # todo work in progress
+def tns(name):  # ticker name system  # todo work in progress, add ETF searching support 2nd indexed
     related_tickers = []
     for key in tickers.keys():
         for ticker in tickers[key]:
@@ -43,7 +43,32 @@ def tns(name):  # ticker name system  # todo work in progress
                 if ticker[0] == _ticker[0]:
                     relevant_indexes.append(key)
 
-    return related_tickers, relevant_indexes
+    if len(related_tickers) == 0:
+        return "ERROR: Ticker not found"
+    else:
+        return related_tickers, relevant_indexes
+
+
+# extra TNS code to try to detect if invalid ticker is returned from TNS and fix ticker
+# code also returns ticker data
+def tns_check(ticker, name):
+    ticker_live = get_ticker_data(ticker)
+    fixed = True
+    try:
+        ticker_live['Open']
+    except KeyError:
+        fixed = False
+    for key in ticker_live.keys():
+        if re.search(r"\b"+re.escape(name.lower())+r"\b", ticker_live[key].lower()):
+            print(f"TNS Check fixed: {ticker[0][0]} --> {key}")
+            ticker = [[key, ticker_live[key]]]
+            ticker_live = get_ticker_data(ticker[0][0])
+            fixed = True
+            break
+    if not fixed:
+        print(ticker_live)
+        exit(f"Ticker error: TNS Check failed to resolve ticker")
+    return ticker_live
 
 
 def _ticker_info_writer_(_ticker):
@@ -89,6 +114,7 @@ def load_ticker_info(_ticker):
             file_time = datetime.strptime(f.readline().split("+")[2].replace("\n", ""), "%Y-%m-%d %H:%M:%S.%f")
             if file_time < datetime.now():
                 ticker_profile = _ticker_info_writer_(_ticker)
+                print(f"Reloaded ticker profile for {_ticker}")
             else:
                 ticker_profile = {}
                 for line in f.readlines():
@@ -101,25 +127,6 @@ def load_ticker_info(_ticker):
     return ticker_profile
 
 
-# todo refresh profile caches out of date
-exec_dict = {}
-for folder in listdir("TickerData/Tickers"):
-    if exists(f"TickerData/Tickers/{folder}/profile.txt"):
-        with open(f"TickerData/Tickers/{folder}/profile.txt", "r", encoding="utf-8") as f:
-            ticker_profile = load_ticker_info(folder)
-            if "companyOfficers" in ticker_profile:
-                exec_dict.update({folder: eval(ticker_profile["companyOfficers"])})
-
-
-def get_exec_data():
-    return exec_dict
-
-
-# lse = ["SHRS", "ETFS", "DPRS", "OTHR"]
-# todo detect type of ticker loaded
-# todo improve TNS for ETF type finder, eg ISF.L working (basically checking 2nd index)
-# nasdaq = ??? a mess of like 400 different types, more research needed
-
 # code to generate ticker profile cache
 #counter = 0
 #for ticker_name in nasdaq:
@@ -128,90 +135,110 @@ def get_exec_data():
 #    print(counter)
 #input()
 
+print("Loading profile data...")
+exec_dict = {}
+comp_names_l = {}
+comp_names_s = {}
+for folder in listdir("TickerData/Tickers"):
+    if exists(f"TickerData/Tickers/{folder}/profile.txt"):
+        with open(f"TickerData/Tickers/{folder}/profile.txt", "r", encoding="utf-8") as f:
+            ticker_profile = load_ticker_info(folder)
+            if "companyOfficers" in ticker_profile:
+                exec_dict.update({folder: eval(ticker_profile["companyOfficers"])})
+            if "longName" in ticker_profile:
+                comp_names_l.update({folder: ticker_profile["longName"]})
+            if "shortName" in ticker_profile:
+                comp_names_s.update({folder: ticker_profile["shortName"]})
+
+
+def get_exec_data():
+    return exec_dict
+
+
+def get_comp_names_l():
+    return comp_names_l
+
+
+def get_comp_names_s():
+    return comp_names_s
+
+
+print("Loaded profile data successfully...\nFinished loading ticker_loader.py...\n"
+      "-------------------------------------------")
+
+
+if __name__ == "__main__":
+    print("File loaded directly, running tests...")
+    # lse = ["SHRS", "ETFS", "DPRS", "OTHR"]
+    # todo detect type of ticker loaded
+    # todo improve TNS for ETF type finder, eg ISF.L working (basically checking 2nd index)
+    # nasdaq = ??? a mess of like 400 different types, more research needed
+
+    while True:
+        c_name = input("Ticker name: ")
+        c_ticker, c_index = tns(c_name)
+        if not c_ticker:
+            print("Ticker not found: TNS failed to resolve ticker")
+        else:
+            break
+    print(c_ticker[0][0], c_index)
+
+    ticker_data = tns_check(c_ticker[0][0], c_name)
+
+    print(c_ticker[0][0], c_index)
+    print(ticker_data)
+    ticker_stats = get_ticker_stats(c_ticker[0][0])
+    if "Previous Close" in ticker_stats.keys():
+        print("Ticker doesnt have stats")
+        ticker_stats = {}
+    else:
+        print(ticker_stats)
+
+    ticker_data = load_ticker_info(c_ticker[0][0])  # loads from cache or generates cache
+    # todo if ticker_data = {} deal with error
+    print(ticker_data)
+
+    #input("Enter to fetch all data: ")
+    #t_object = yf.Ticker(ticker[0][0])
+
+    #print(t_object.news)  # << live
+    #print(t_object.earnings_dates)
+    #print(t_object.dividends.values)
+    #print(t_object.actions)
+    #print(t_object.balance_sheet)
+    #print(t_object.capital_gains)  # ???
+    #print(t_object.cash_flow)
+    #print(t_object.financials)
+    #print(t_object.history_metadata)
+    #print(t_object.income_stmt)
+    #print(t_object.institutional_holders)
+    #print(t_object.mutualfund_holders)
+    #print(t_object.quarterly_balance_sheet)
+    #print(t_object.quarterly_cash_flow)
+    #print(t_object.quarterly_financials)
+    #print(t_object.quarterly_income_stmt)
+    #print(t_object.splits)
+
+    #print(ticker_data)
+
+    #input("FINISHED.")
+    #print(get_ticker_history("tsla", datetime.now()-timedelta(days=1), datetime.now(), "1d", "1m"))
 
 
 
-# the below block of code is TNS logic
-#while True:
-#    c_name = input("Ticker name: ")
-#    ticker, index = tns(c_name)
-#    if not ticker:
-#        print("Ticker not found: TNS failed to resolve ticker")
-#    else:
-#        break
+    #print(get_yf_rss('tsla'))
 
-#ticker_live = get_ticker_data(ticker[0][0])
-# extra ticker name system code to try to detect if invalid ticker is returned from TNS
-#try:
-#    ticker_live['Open']
-#except KeyError:
-#    fixed = False
-#    for key in ticker_live.keys():
-#        if re.search(r"\b"+re.escape(c_name.lower())+r"\b", ticker_live[key].lower()):
-#            print(f"TNS Fixed: {ticker[0][0]} --> {key}")
-#            ticker = [[key, ticker_live[key]]]
-#            ticker_live = get_ticker_data(ticker[0][0])
-#            fixed = True
-#            break
-#    if not fixed:
-#        print(ticker_live)
-#        exit(f"Ticker error: TNS failed to resolve ticker")
-# the above block of code is TNS logic
-
-#print(ticker[0][0], index)
-#print(ticker_live)
-#ticker_stats = get_ticker_stats(ticker[0][0])
-#if "Previous Close" in ticker_stats.keys():
-#    print("Ticker doesnt have stats")
-#    ticker_stats = {}
-#else:
-#    print(ticker_stats)
-
-#ticker_data = load_ticker_info(ticker[0][0])  # loads from cache or generates cache
-# todo if ticker_data = {} deal with error
-#print(ticker_data)
-
-#input("Enter to fetch all data: ")
-#t_object = yf.Ticker(ticker[0][0])
-
-#print(t_object.news)  # << live
-#print(t_object.earnings_dates)
-#print(t_object.dividends.values)
-#print(t_object.actions)
-#print(t_object.balance_sheet)
-#print(t_object.capital_gains)  # ???
-#print(t_object.cash_flow)
-#print(t_object.financials)
-#print(t_object.history_metadata)
-#print(t_object.income_stmt)
-#print(t_object.institutional_holders)
-#print(t_object.mutualfund_holders)
-#print(t_object.quarterly_balance_sheet)
-#print(t_object.quarterly_cash_flow)
-#print(t_object.quarterly_financials)
-#print(t_object.quarterly_income_stmt)
-#print(t_object.splits)
-
-#print(ticker_data)
-
-#input("FINISHED.")
-#print(get_ticker_history("tsla", datetime.now()-timedelta(days=1), datetime.now(), "1d", "1m"))
+    # get live price of apple
+    #print(get_live_price('aapl'))
 
 
-
-#print(get_yf_rss('tsla'))
-
-# get live price of apple
-#print(get_live_price('aapl'))
+    #print(tickers_sp500())
 
 
-#print(tickers_sp500())
+    #url = "https://www.google.com/finance/quote/TSLA:NASDAQ"
+    #print(get(url).text)
 
 
-#url = "https://www.google.com/finance/quote/TSLA:NASDAQ"
-#print(get(url).text)
-
-
-# live price sources:
-# https://www.google.com/finance/quote/TSLA:NASDAQ
-# https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch
+    # live price sources:
+    # https://www.google.com/finance/quote/TSLA:NASDAQ
+    # https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch
